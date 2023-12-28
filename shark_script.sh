fa796ffdf7a004f415a40ab9dbf3c248b0ddae0b
= <=>
2023-12-28 12:23:39 +0100

    update shark script

--- shark_script.sh
+++ shark_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=barlow-twins-training
+#SBATCH --job-name=mae-pretraining
 #SBATCH --partition=PATHgpu
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=3
 #SBATCH --gres=gpu:3
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128gb
-#SBATCH --chdir=/exports/path-nefro-hpc/gbuzzanca_/Foundation-Models-Pretraining/
-#SBATCH --output=/exports/path-nefro-hpc/gbuzzanca_/Foundation-Models-Pretraining/%x-%j.out
-#SBATCH --error=/exports/path-nefro-hpc/gbuzzanca_/Foundation-Models-Pretraining/%x-%j.err
+#SBATCH --chdir=/exports/path-nefro-hpc/gbuzzanca_/MAE/
+#SBATCH --output=/exports/path-nefro-hpc/gbuzzanca_/MAE/%x-%j.out
+#SBATCH --error=/exports/path-nefro-hpc/gbuzzanca_/MAE/%x-%j.err
 
 export PYTHONUNBUFFERED=TRUE
 export NCCL_SOCKET_IFNAME=team0
@@ -17,6 +17,6 @@ module load library/cuda/11.6.1/gcc.8.3.1
 module load library/cudnn/11.6/cudnn
 
 source ~/.bashrc
-conda activate /exports/path-nefro-hpc/gbuzzanca_/.conda/envs/barlow_twins
+conda activate /exports/path-nefro-hpc/gbuzzanca_/.conda_envs/mae
 
-srun python train.py --images-folders=/exports/path-nefro-hpc/PATCHES_REST/AMC --images-folders=/exports/path-nefro-hpc/PATCHES_REST/KUL --images-folders=/exports/path-nefro-hpc/PATCHES_REST/LUMC --images-folders=/exports/path-nefro-hpc/PATCHES_REST/UMCU --images-folders=/exports/path-nefro-hpc/PATCHES_LANCET/TRAIN --batch-size=1152
+srun python submit_pretrain.py --job_dir ${JOB_DIR} --norm_pix_loss --data_path /export/path-nefro-hpc/gbuzzanca_/train_amc_umcu_lumc_kul_test_uka_runmc/train
