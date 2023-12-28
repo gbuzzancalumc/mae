dfa33ce578edd381240607293664a8b96ec44657
Buzzanca <gbuzzanca@res-hpc-lo02.researchlumc.nl>
2023-12-28 12:30:46 +0100

    update shark script

--- shark_script.sh
+++ shark_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=mae-pretraining
 #SBATCH --partition=PATHgpu
-#SBATCH --nodes=3
-#SBATCH --ntasks-per-node=3
-#SBATCH --gres=gpu:3
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=128gb
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64gb
 #SBATCH --chdir=/exports/path-nefro-hpc/gbuzzanca_/mae/
 #SBATCH --output=/exports/path-nefro-hpc/gbuzzanca_/mae/%x-%j.out
 #SBATCH --error=/exports/path-nefro-hpc/gbuzzanca_/mae/%x-%j.err
@@ -17,6 +17,6 @@ module load library/cuda/11.6.1/gcc.8.3.1
 module load library/cudnn/11.6/cudnn
 
 source ~/.bashrc
-conda activate /exports/path-nefro-hpc/gbuzzanca_/.conda_envs/mae
+conda activate /exports/path-nefro-hpc/gbuzzanca_/conda_envs/mae
 
-srun python submit_pretrain.py --job_dir ${JOB_DIR} --norm_pix_loss --data_path /export/path-nefro-hpc/gbuzzanca_/train_amc_umcu_lumc_kul_test_uka_runmc/train
+srun python submitit_pretrain.py --job_dir ${JOB_DIR} --norm_pix_loss --data_path /export/path-nefro-hpc/gbuzzanca_/train_amc_umcu_lumc_kul_test_uka_runmc/train
